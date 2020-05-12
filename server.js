a824ee6ed147dc160d2b23e8170ee9c0438787de
Ewa Czech <ewa.czech@hotmail.com>
2020-05-12 19:39:00 +0200

    add mvp

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
+server.use(express.json());
+server.use(logger);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+	let date = new Date();
+	console.log(
+		`Request method: ${req.method}, request url: ${
+			req.originalUrl
+		} on ${date.toISOString()}`
+	);
+	next();
+}
 
 module.exports = server;
